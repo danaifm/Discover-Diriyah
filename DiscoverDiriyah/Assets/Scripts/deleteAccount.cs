6b4ad58baa992691a4e755127341fd743489fe9f
Bdoor Nasser <bdoorna7@gmail.com>
2024-02-26 00:42:50 +0300

    done

--- DiscoverDiriyah/Assets/Scripts/deleteAccount.cs
+++ DiscoverDiriyah/Assets/Scripts/deleteAccount.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Firebase;
 using Firebase.Auth;
+using UnityEngine.Networking;
 using System;
 using Firebase.Firestore;
 using Vuforia;
@@ -13,14 +14,30 @@ public class deleteAccount : MonoBehaviour
     public FirebaseAuth auth;
     public FirebaseUser user;
     public string userID;
+
    
-    public  void DeleteAccount()
+
+        public void DeleteAccount()
     {
         auth = FirebaseAuth.DefaultInstance;
         user = auth.CurrentUser;
         userID = user.UserId;
+        //userID = "ATpoagFEmbZgeK6HP6jwwb6VT7B2";
 
-       // DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+        DocumentReference docRef = db.Collection("users").Document(userID);
+
+        docRef.DeleteAsync().ContinueWith(task =>
+        {
+            if (task.IsCompleted)
+            {
+                Debug.Log("Account document deleted successfully!" + userID);
+            }
+            else if (task.IsFaulted)
+            {
+                Debug.LogError("Failed to delete account document: " + task.Exception);
+            }
+        });
 
 
 
