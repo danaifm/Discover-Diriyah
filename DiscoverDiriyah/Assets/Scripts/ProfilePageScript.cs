b028c0f7d982d217f49879c6ca57f05b060d477d
Dana Almoaiqel <113850532+danaifm@users.noreply.github.com>
2024-02-29 17:50:20 +0300

    :)

--- DiscoverDiriyah/Assets/Scripts/ProfilePageScript.cs
+++ DiscoverDiriyah/Assets/Scripts/ProfilePageScript.cs
@@ -1,14 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Firestore;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ProfilePageScript : MonoBehaviour
 {
+    public FirebaseAuth auth;
+    public FirebaseUser user;
+    private FirebaseApp app;
+    private CollectionReference fs;
+    private DocumentReference userinfo;
+    private DocumentSnapshot snapshot;
+    private Dictionary<string, object> dictionary;
+    [SerializeField] public TMP_Text nameText, emailText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("1. in start");
+        initializeFirebase();
+        fs = FirebaseFirestore.DefaultInstance.Collection("Account");
+        Debug.Log("4. before getuserinfo async");
+        userinfo = fs.Document(user.UserId);
+        getUserInfo(userinfo);
+        Debug.Log("7. in start after getuserinfo async");
+    }
+
+    public async void getUserInfo(DocumentReference userinfo)
+    {
+        Debug.Log("5. in getuserinfo async");
+        snapshot = await userinfo.GetSnapshotAsync();
+        Debug.Log("6. end of getuserinfo async");
+        dictionary = snapshot.ToDictionary();
+        Debug.Log("8. printing dictionary");
+        Debug.Log(dictionary["Name"].ToString());
+        Debug.Log(dictionary["Email"].ToString());
+        nameText.text = dictionary["Name"].ToString();
+        emailText.text = dictionary["Email"].ToString();
+    }
+
+    void initializeFirebase()
+    {
+        Debug.Log("2. in initialize firebase");
+        app = FirebaseApp.DefaultInstance;
+        auth = FirebaseAuth.DefaultInstance;
+        user = auth.CurrentUser;
+        Debug.Log("3. end of initialize firebase");
     }
 
     // Update is called once per frame
