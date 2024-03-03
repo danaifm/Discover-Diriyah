dbcda7d8c52ef58867c930e32754c7491084002c
Dana Almoaiqel <113850532+danaifm@users.noreply.github.com>
2024-03-03 17:51:24 +0300

    confirmation message for edit acc

---
+++ DiscoverDiriyah/Assets/Scripts/ConfirmationScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ConfirmationScript : MonoBehaviour
+{
+    public Button yesButton, noButton;
+    public TextMeshProUGUI messageText;
+
+}
