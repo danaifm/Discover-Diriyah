833ce31e9836a689f72d80038ce4e39ff575be3f
Bdoor Nasser <bdoorna7@gmail.com>
2024-03-24 22:35:20 +0300

    مدرييي

---
+++ DiscoverDiriyah/Assets/SceneLoader.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+   public string sceneName;
+
+    public void GoNextScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+}
